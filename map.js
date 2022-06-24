a1ef32c03ee113032289f9b9daa5f409ab826c17
Fritz Latagan <gerald.latagan@gmail.com>
2022-06-24 15:09:38 +0800

    Update map.js
    added variables and values that make up the leaflet map

---
+++ map.js
@@ -0,0 +1,21 @@
+function initMap() {
+  let centerpoint = [1.3521, 103.8191];
+  let map = L.map("map");
+  map.setView(centerpoint, 10);
+
+  let tileLayer = L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <ahref="https://www.openstreetmap.org">OpenStreetMap<a> contributors, <ahref="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery(c) <a href="https://www.mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw", //demo access token (shared by a lot of people)
+    }
+  ).addTo(map);
+
+  return map;
+}
