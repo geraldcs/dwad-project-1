68225b59197dd4b9fad100d9057649a856b4064b
Fritz Latagan <gerald.latagan@gmail.com>
2022-07-16 21:40:21 +0800

    Update result.js
    added custom icons for markers

--- scripts/result.js
+++ scripts/result.js
@@ -3,11 +3,27 @@ function searchResult(map, result, resultLayer) {
         result.geocodes.main.latitude,
         result.geocodes.main.longitude,
     ];
-    let markerResults = L.marker(latlng);
+    // custom icons
+    let customIcons = L.icon({
+        iconUrl: `${result.categories[0].icon.prefix}32${result.categories[0].icon.suffix}`,
+        iconSize: [40, 90], 
+        iconAnchor: [22, 54], 
+        popupAnchor: [-3, -76]
+      })
+      let markerColor = document.querySelectorAll('.leaflet-marker-icon');
+      for (let m of markerColor) {
+        m.style.borderColor = 'black';
+        m.style.filter = 'brightness(0)';
+        m.style.filter = 'invert(43%) sepia(48%) saturate(636%) hue-rotate(78deg) brightness(86%) contrast(97%) drop-shadow(1px 1px 0 black)';
+        
+      }
+
+    let markerResults = L.marker(latlng, {icon: customIcons});
 
     markerResults.bindPopup(`
         <h4 style="font-family: TeXGyreAdventor; font-weight: bold">${result.name}</h4>
-        <h6 style="font-family: TeXGyreAdventor;">${result.location.formatted_address}</h6>
+        <h5 style="font-family: TeXGyreAdventor;">(${result.categories[0].name})</h5>
+        <h6 style="font-family: TeXGyreAdventor; font-size: 15px">${result.location.formatted_address}</h6>
     `)
 
     markerResults.addTo(resultLayer);
